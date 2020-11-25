c52fe5be1ed9e64db80c28e3df99d34a971b7536
ordobayev <ordobayev@gmail.com>
2020-11-25 07:53:46 +0600

    Rename ViewController to SearchViewController and Modified Storyboard from iphone 12 to Iphone 12 pro

--- FakeItunes/SearchViewController.swift
+++ FakeItunes/SearchViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,3 +17,6 @@ class ViewController: UIViewController {
 
 }
 
+// M - Modified
+// R - Renamed
+// Commit
